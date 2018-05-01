540793d4d70ad0ec4741797fbd8ab5b8eef85aea
Raymond Franco <raymond@almostaninja.com>
2018-05-01 09:03:26 +0100

    Added comments

--- VMware/Connect-To-Multiple-vCenter-Instances.ps1
+++ VMware/Connect-To-Multiple-vCenter-Instances.ps1
@@ -1,7 +1,10 @@
+# This will prompt you for credentials
 $Credentials = Get-Credential
 
+#Change the below to reflect your vCenter information or comment / add additional variables if you wish
 $vserver1 = "vcenter1.domain.local"
 $vserver2 = "vcenter2.domain.local"
 $vserver3 = "vcenter3.domain.local"
 
+#The connection string. Remember if you comment/add additional variables above, you would need to edit below. 
 Connect-VIServer "$vserver1","$vserver2","$vserver3" -Credential $Credentials
