cb15eca28d27ad4a38bb36260667e341b734417c
Raymond Franco <raymond@almostaninja.com>
2018-05-03 11:28:41 +0100

    removed test commit

--- VMware/Connect-To-Multiple-vCenter-Instances.ps1
+++ VMware/Connect-To-Multiple-vCenter-Instances.ps1
@@ -6,5 +6,5 @@ $vserver1 = "vcenter1.domain.local"
 $vserver2 = "vcenter2.domain.local"
 $vserver3 = "vcenter3.domain.local"
 
-#The connection string. Remember if you comment/add additional variables above, you would need to edit below.. 
+#The connection string. Remember if you comment/add additional variables above, you would need to edit below.
 Connect-VIServer "$vserver1","$vserver2","$vserver3" -Credential $Credentials
